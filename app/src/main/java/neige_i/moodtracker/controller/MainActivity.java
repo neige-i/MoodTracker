30fefb60007b08a80f7062911b26804232cd04d5
neige-i <ibrahim.naji.rachdi@gmail.com>
2017-10-22 21:06:49 +0200

    AlertDialog created with a TextInputLayout inside

--- app/src/main/java/neige_i/moodtracker/controller/MainActivity.java
+++ app/src/main/java/neige_i/moodtracker/controller/MainActivity.java
@@ -1,15 +1,20 @@
 package neige_i.moodtracker.controller;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import fr.castorflex.android.verticalviewpager.VerticalViewPager;
 import neige_i.moodtracker.R;
 import neige_i.moodtracker.model.Mood;
 import neige_i.moodtracker.model.MoodPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private VerticalViewPager mMoodPager;
 
@@ -21,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Mood.initDrawables();
         Mood.initColours();
 
-        mMoodPager = (VerticalViewPager) findViewById(R.id.mood_pager);
+        mMoodPager = findViewById(R.id.mood_pager);
         mMoodPager.setAdapter(new MoodPagerAdapter(getSupportFragmentManager()));
         mMoodPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
@@ -30,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mMoodPager.setCurrentItem(Mood.DEFAULT_MOOD);
+
+        findViewById(R.id.new_note_ic).setOnClickListener(this);
+        findViewById(R.id.history_ic).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.new_note_ic:
+                AlertDialog commentaryDialog = new AlertDialog.Builder(this)
+                        .setView(R.layout.text_input_layout)
+                        .setPositiveButton(R.string.dialog_positive_btn, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Save commentary", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(R.string.dialog_negative_btn, null)
+                        .create();
+                commentaryDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                commentaryDialog.show();
+                break;
+            case R.id.history_ic:
+                Toast.makeText(MainActivity.this, "Start activity", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
