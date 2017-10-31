335f21da007c4f761a044cfcda35e4ca3c749e68
neige-i <ibrahim.naji.rachdi@gmail.com>
2017-10-31 13:29:55 +0100

    Complete PrefUpdateReceiver class

--- app/src/main/java/neige_i/moodtracker/model/PrefUpdateReceiver.java
+++ app/src/main/java/neige_i/moodtracker/model/PrefUpdateReceiver.java
@@ -3,6 +3,13 @@ package neige_i.moodtracker.model;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.util.Calendar;
+
+import static neige_i.moodtracker.controller.MainActivity.PREF_KEY_MOOD;
+import static neige_i.moodtracker.model.History.DAY_COUNT;
 
 /**
  * This Receiver does only one thing: update the preferences.
@@ -12,7 +19,26 @@ public class PrefUpdateReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.getSharedPreferences("controller.MainActivity", Context.MODE_PRIVATE)
-                .edit().remove("mood").apply();
+        SharedPreferences preferences = context.getSharedPreferences("controller.MainActivity", Context.MODE_PRIVATE);
+
+        int currentDay = Calendar.getInstance().get(Calendar.DATE);
+        Log.i("Receiver", "Day " + currentDay);
+        if (preferences.getInt("Current day", 0) == 0)
+            preferences.edit().putInt("Current day", currentDay).apply();
+        else if (preferences.getInt("Current day", currentDay) != currentDay) {
+            int i = 0;
+            SharedPreferences.Editor editor = preferences.edit();
+            String mood;
+
+            while ((mood = preferences.getString(PREF_KEY_MOOD + (i++), null)) != null)
+                if (i <= DAY_COUNT)
+                    editor.putString(PREF_KEY_MOOD + i, mood);
+            editor.putString(PREF_KEY_MOOD + 0, new Mood().toString());
+
+            editor.putInt("Current day", currentDay);
+
+            editor.apply();
+            Log.i("Receiver", "update prefs");
+        }
     }
 }
