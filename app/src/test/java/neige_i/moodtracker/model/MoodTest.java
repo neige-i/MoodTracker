60277145193e1ef82fa8d72815ba7cf86e84e20d
neige-i <ibrahim.naji.rachdi@gmail.com>
2017-11-12 20:58:09 +0100

    Add comentaries to unit tests

--- app/src/test/java/neige_i/moodtracker/model/MoodTest.java
+++ app/src/test/java/neige_i/moodtracker/model/MoodTest.java
@@ -5,74 +5,107 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MoodTest {
-    // -------------------------------------     INSTANCE VARIABLES     -------------------------------------
-
-    private Mood mActualMood;
-    private Mood mExpectedMood;
-    private boolean mIsValid;
-
     // ----------------------------------------     TEST METHODS     ----------------------------------------
 
+    // To test the setSmiley and setCommentary methods, we just assign them a valid and non valid value.
+
+    /**
+     * Tests the {@link Mood#setSmiley(int)  setSmiley} method with a valid argument.
+     * @throws Exception
+     */
     @Test
     public void setSmiley_valid() throws Exception {
         assertTrue(isSmileyValid(4));
     }
 
+    /**
+     * Tests the {@link Mood#setSmiley(int)  setSmiley} method with a non valid argument.
+     * @throws Exception
+     */
     @Test
     public void setSmiley_outOfRange_invalid() throws Exception {
         assertFalse(isSmileyValid(10));
     }
 
+    /**
+     * Tests the {@link Mood#setCommentary(String)  setCommentary} method with a valid argument.
+     * @throws Exception
+     */
     @Test
     public void setCommentary_valid() throws Exception {
         assertTrue(isCommentaryValid("a commentary"));
     }
 
+    /**
+     * Tests the {@link Mood#setCommentary(String)  setCommentary} method with a non valid argument.
+     * @throws Exception
+     */
     @Test
     public void setCommentary_null_invalid() throws Exception {
         assertFalse(isCommentaryValid(null));
     }
 
+    // To test the fromString method, we compare its returned value with
+    // a Mood object which is instantiated with its constructor.
+    // This constructor takes the expected values for the smiley and the commentary.
+
+    /**
+     * Tests the {@link Mood#fromString(String) fromString} method with a String argument that represents
+     * a mood with a commentary.
+     * @throws Exception
+     */
     @Test
-    public void fromString_fullMood() throws Exception {
-        mActualMood = Mood.fromString("4Feel great");
-        mExpectedMood = new Mood(4, "Feel great");
-        assertEquals(mExpectedMood, mActualMood);
+    public void fromString_moodWithCommentary() throws Exception {
+        assertEquals(new Mood(4, "Feel great"), Mood.fromString("4Feel great"));
     }
 
+    /**
+     * Tests the {@link Mood#fromString(String) fromString} method with a String argument that represents
+     * a mood without commentary.
+     * @throws Exception
+     */
     @Test
     public void fromString_moodWithoutCommentary() throws Exception {
-        mActualMood = Mood.fromString("2");
-        mExpectedMood = new Mood(2, "");
-        assertEquals(mExpectedMood, mActualMood);
+        assertEquals(new Mood(2, ""), Mood.fromString("2"));
     }
 
+    /**
+     * Tests the {@link Mood#fromString(String) fromString} method with a String argument that represents
+     * an empty mood.
+     * @throws Exception
+     */
     @Test
     public void fromString_emptyMood() throws Exception {
-        mActualMood = Mood.fromString("9");
-        mExpectedMood = new Mood();
-        assertEquals(mExpectedMood, mActualMood);
+        assertEquals(new Mood(), Mood.fromString("9"));
     }
 
     // ---------------------------------------     PRIVATE METHODS     --------------------------------------
 
+    /**
+     * Returns true if the specified smiley is valid, false otherwise.
+     * @param smiley The smiley to test.
+     * @return true if smiley is valid, false otherwise.
+     */
     private boolean isSmileyValid(int smiley) {
         try {
             new Mood().setSmiley(smiley);
-            mIsValid = true;
+            return true;
         } catch (IndexOutOfBoundsException e) {
-            mIsValid = false;
+            return false;
         }
-        return mIsValid;
     }
 
+    /**
+     * Returns true if the specified commentary is valid, false otherwise.
+     * @param commentary The commentary.
+     * @return true if commentary is valid, false otherwise.
+     */
     private boolean isCommentaryValid(String commentary) {
         try {
             new Mood().setCommentary(commentary);
-            mIsValid = true;
+            return true;
         } catch (IllegalArgumentException e) {
-            mIsValid = false;
+            return false;
         }
-        return mIsValid;
     }
 }
